b62e4781ce3e1e6b43a04574e39e54052e174765
Ivan Tishchenko <moodminds@hotmail.com>
2022-03-16 12:44:41 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# origins-spring
-Origins Spring - Registration of the Origins' reactive interfaces in Spring Context.
+# Registration of the [Origins](https://github.com/MoodMinds/origins)' reactive interfaces in [Spring](https://spring.io)
+
+Registration of the [Origins](https://github.com/MoodMinds/origins)' `Emittable` and `Publishable`
+in Spring's `ReactiveAdapterRegistry` for Reactive context.
+
+## Usage
+
+Include provided `ReactiveAdapterRegistration` in your Spring configuration.
+
+## Maven configuration
+
+Artifacts can be found on [Maven Central](https://search.maven.org/) after publication.
+
+```xml
+<dependency>
+    <groupId>org.moodminds</groupId>
+    <artifactId>origins-spring</artifactId>
+    <version>${version}</version>
+</dependency>
+```
+
+## Building from Source
+
+You may need to build from source to use **Origins Spring** (until it is in Maven Central) with Maven and JDK 9 at least.
+
+## License
+This project is going to be released under version 2.0 of the [Apache License][l].
+
+[l]: https://www.apache.org/licenses/LICENSE-2.0
---
+++ src/main/java/org/moodminds/spring/ReactiveAdapterRegistration.java
@@ -0,0 +1,63 @@
+package org.moodminds.spring;
+
+import org.moodminds.lang.Emittable;
+import org.moodminds.lang.Publishable;
+import org.moodminds.reactor.adapter.PublishableAdapter;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ReactiveAdapterRegistry;
+
+import static org.moodminds.lang.Emittable.emittable;
+import static org.moodminds.lang.Publishable.publishable;
+import static org.moodminds.reactor.adapter.PublishableAdapter.fromReactive;
+import static org.moodminds.reactor.adapter.ReactorPublisherAdapter.toReactor;
+import static org.springframework.core.ReactiveTypeDescriptor.multiValue;
+
+/**
+ * The {@link Emittable} Reactive Adapter Registration configuration bean.
+ */
+@Configuration
+public class ReactiveAdapterRegistration implements InitializingBean {
+
+    /**
+     * The reactive adapter registry bean holder field.
+     */
+    private final ReactiveAdapterRegistry reactiveAdapterRegistry;
+
+    /**
+     * Construct the configuration bean object with the specified {@link ReactiveAdapterRegistry}.
+     *
+     * @param reactiveAdapterRegistry the specified {@link ReactiveAdapterRegistry}
+     */
+    public ReactiveAdapterRegistration(@Autowired(required = false) ReactiveAdapterRegistry reactiveAdapterRegistry) {
+        this.reactiveAdapterRegistry = reactiveAdapterRegistry;
+    }
+
+    /**
+     * Register the {@link Emittable} in the available {@link ReactiveAdapterRegistry}
+     * instances at the end of the bean initialization.
+     */
+    @Override
+    public void afterPropertiesSet() {
+        this.registerAdapter(ReactiveAdapterRegistry.getSharedInstance());
+        if (this.reactiveAdapterRegistry != null) this.registerAdapter(this.reactiveAdapterRegistry);
+    }
+
+    /**
+     * Register the {@link Emittable} in the specified {@link ReactiveAdapterRegistry}.
+     */
+    private void registerAdapter(ReactiveAdapterRegistry reactiveAdapterRegistry) {
+        reactiveAdapterRegistry.registerReactiveType(
+            multiValue(Emittable.class, () -> emittable(publishable())),
+            publishable -> toReactor(((Emittable<?, ?>) publishable)::subscribe),
+            publisher -> emittable(fromReactive(publisher))
+        );
+
+        reactiveAdapterRegistry.registerReactiveType(
+            multiValue(Publishable.class, Publishable::publishable),
+            publishable -> toReactor(((Publishable<?, ?>) publishable)::subscribe),
+            PublishableAdapter::fromReactive
+        );
+    }
+}
